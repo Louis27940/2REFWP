194e7d462d96d9dc3b48d67e4aa04c4b4ada6037
Louis27940 <louis.siimon@gmail.com>
2025-12-15 15:39:36 +0100

    ajout du module spped insight de vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -30,7 +31,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fr" className="dark">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
